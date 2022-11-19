b3d430063f833cb39620d578463999d9d1ba2c7c
Nick Miskelin <nick@mail.com>
2022-11-18 22:18:08 -0500

    fixed icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,27 +20,88 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
     >
       <span style={{ background: dotColor }}
         className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'
-      >
+      />
         {icon}
-      </span>
     </button>
   </TooltipComponent>
 )
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize } = useStateContext();
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, [])
+
+  useEffect(() => {
+    if(screenSize <= 900){
+      setActiveMenu(false)
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize])
 
   return (
     <div className='flex justify-between p-2 md:mx-6 relative'>
-      <NavButton title='Menu'
+      <NavButton
+        title='Menu'
         customFunc={() => setActiveMenu((prevActiveMenu) =>
-          !prevActiveMenu)} color='blue' icon={<AiOutlineMenu />}
+          !prevActiveMenu)}
+        color='blue'
+        icon={<AiOutlineMenu />}
       />
       <div className='flex'>
-        <NavButton title='Menu'
-          customFunc={() => setActiveMenu((prevActiveMenu) =>
-            !prevActiveMenu)} color='blue' icon={<AiOutlineMenu />}
+        <NavButton
+          title='Cart'
+          customFunc={() => handleClick('cart')}
+          color='blue'
+          icon={<FiShoppingCart />}
         />
+        <NavButton
+          title='Chat'
+          dotColor="#03C97"
+          customFunc={() => handleClick('chat')}
+          color='blue'
+          icon={<BsChatLeft />}
+        />
+        <NavButton
+          title='Notifications'
+          dotColor="#03C97"
+          customFunc={() => handleClick('notification')}
+          color='blue'
+          icon={<RiNotification3Line />}
+        />
+        <TooltipComponent
+          content='profile'
+          position='BottomCenter'
+        >
+          <div className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg'
+            onClick={() => handleClick('userProfile')}
+          >
+            <img 
+              className='rounded-full w-8 h-8'
+              src={avatar}
+            />
+            <p>
+              <span className='text-gray-400 text-14'>Hi, </span> {' '}
+              <span className='text-gray-400 font-bold ml-1 text-14'>Michael</span>
+            </p>
+            <MdKeyboardArrowDown 
+              className='text-gray-400 text-14'
+            />
+          </div>
+        </TooltipComponent>
+
+        {isClicked.cart && <Cart />}
+        {isClicked.chat && <Chat />}
+        {isClicked.notification && <Notification />}
+        {isClicked.userProfile && <UserProfile />}
       </div>
     </div>
   )
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -13,12 +13,26 @@ const initialState = {
 export const ContextProvider = ({ children }) => {
 
     const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState);
+    const [screenSize, setScreenSize] = useState(undefined);
+
+    const handleClick = (clicked) => {
+        // Open up the object and spread the initial state in order to determine which value
+        // within the object has been clicked and change that value
+        // This is because the setIsClicked object cannot be overriden with a string
+        setIsClicked({ ...initialState, [clicked]:true});
+    }
 
     return (
         <StateContext.Provider
             value={{
                 activeMenu,
                 setActiveMenu,
+                isClicked,
+                setIsClicked,
+                handleClick,
+                screenSize,
+                setScreenSize
             }}
         >
             {children}
