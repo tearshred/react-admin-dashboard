ee34fe699339a2bcac98f858389907e723ef21cd
Nick Miskelin <nick@mail.com>
2022-11-22 13:31:42 -0500

    header

--- src/pages/FinancialReport.jsx
+++ src/pages/FinancialReport.jsx
@@ -16,15 +16,22 @@ const FinancialReport = () => {
     return (
         // Body
         <div className='mt-12 mb-28 max-w-screen-xl m-auto'>
+
+            {/* Header */}
+            <div className='text-center'>
+                <h1 className='dark:text-white text-3xl font-bold text-gray-800'>Company Inc.</h1>
+            </div>
+
+            {/* Main Container */}
             <div className='flex flex-wrap lg:flex-nowrap justify-center'>
 
-                {/* Main Containr */}
                 <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg
-        rounded-xl w-full p-8 pt-9 m-4'>
+                rounded-xl w-full p-8 pt-9 m-4'>
 
                     {/* Top Grid, Financial Overview */}
                     <div className='grid grid-cols-4 gap-4'>
 
+                        {/* Mapping through an array of objects and obtaining their values */}
                         {financialData.map((item) => (
                             <div key={item.title}>
                                 {item.title}
